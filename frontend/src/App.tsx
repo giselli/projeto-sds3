1fd6ae37dee2e7869ea57b989d818718d27e7afe
Giselli Hara <gi@hara.ninja>
2021-05-05 09:53:08 -0300

    Security Config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,17 +12,17 @@ function App() {
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <div className="row px-3"> 
+        <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de Sucesso (%) </h5>
             <BarChart />
           </div>
-        
 
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Todas as vendas </h5>
-          <DonutChart />
-        </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas </h5>
+            <DonutChart />
+          </div>
         </div>
 
         <div className="py-3">
