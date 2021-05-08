e1e003afbb2d5ea6b4a1fab834488d7f37ee3c60
Giselli Hara <gi@hara.ninja>
2021-05-08 17:38:48 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -9,23 +10,26 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    let chartData : ChartData = { labels: [], series : []};
-    
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            chartData = { labels: myLabels, series : mySeries};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-            console.log(chartData);
-        });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    },[]);
+    //useEffect executa novamente quando mudar o valor do que vem depois da ,
+
 
     /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     } */
-    
+
     const options = {
         legend: {
             show: true
@@ -33,14 +37,13 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{...options,labels:chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
         />
     );
-  }
-  
-  export default DonutChart;
-  
+}
+
+export default DonutChart;
