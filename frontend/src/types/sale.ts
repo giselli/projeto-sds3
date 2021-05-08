dcf701230729de099d09cb1db715cde7aca749bb
Giselli Hara <gi@hara.ninja>
2021-05-08 19:56:34 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  number: number;
+  numberOfElements?: number;
+  size?: number;
+  empty?: boolean;
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
 };
 
 export type SaleSuccess = {
-    sellerName: string;
-    visited: number;
-    deals: number;
-}
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
